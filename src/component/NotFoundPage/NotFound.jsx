526b760b1e2959d7d474ff624c68a26986183292
unknown <afnanmahmood52@gmail.com>
2021-02-20 17:06:49 +0500

    404 error image

--- src/component/NotFoundPage/NotFound.jsx
+++ src/component/NotFoundPage/NotFound.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import './NotFound.css'
 import "../../../node_modules/aos/dist/aos.css"
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ErrorImage from '../../images/404_Error.png'
+
 
 export default function NotFound() {
   return (
     <div className="container">
         <div className="row">
             <div className="col-sm-12">
-                <div className="not-found-heading">
+                {/* <div className="not-found-heading">
                     <p>Page Not Found!</p>
+                </div> */}
+                <div className="error-image">
+                    <img src={ErrorImage} alt={'error image'}/>
                 </div>
             </div>
         </div>
